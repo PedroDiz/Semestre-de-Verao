6f64eb3748b8d4f27f1cf04811a63c9875489183
PedroDiz <A47718@alunos.isel.pt>
2023-03-04 15:15:05 +0000

    commits á´miguel

--- IA/Trabalhos de casa/Exe01/Ex1.pl
+++ IA/Trabalhos de casa/Exe01/Ex1.pl
@@ -15,7 +15,7 @@
    grandson(X,Y) :- parent(Y,Z), parent(Z,X).
    
    descendent(X,Y) :- parent(Y,X).
-   descendent(X,Y) :- parent(Y,Z), descendent(Z,W).
+   descendent(X,Y) :- parent(X,Z), descendent(Z,Y).
    
    cousin(X,Y) :- parent(Z,X), parent(W,Y), brother(Z,W).
    
